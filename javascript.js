cca7ac5e89215f8a64272107956e4f0f078321ae
Austin Gorman <austin.gorman@gmail.com>
2018-05-24 16:42:14 -0500

    fixed a few errors with the first if statement

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 
     for (let i = 0; i <= 100; i++) {
         // Take the current number, divide by 5, and check if the remainder is 0
-        if ([i] % 5 && [i] % 7 === 0) {
-            console.log('Chicken') 
+        if ([i] % 5 === 0 && [i] % 7 === 0) {
+            console.log('ChickenMonkey') 
             } else if ([i] % 7 === 0) {
                 console.log('Monkey')
         } else if ([i] % 5 === 0) {
-                console.log('ChickenMonkey')    
+                console.log('Chicken')    
             } else {
                 console.log([i])
             }
